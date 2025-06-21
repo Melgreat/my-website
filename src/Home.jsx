b7c81d4dc14ccb5eef84e3f5a3178591f2cd8e5c
Melvin <gentlenk1@gmail.com>
2025-06-21 15:37:06 +0100

    Changed the profile image

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import profile from './assets/e035717e-a755-485a-86c7-c165a4f7bb80.jpg';
+import profile from './assets/profile image.jpg';
 import behance from './assets/5305150_behance_portfolio_behance logo_icon.svg';
 import github from './assets/8666686_github_icon.svg';
 import linked from './assets/8679367_linkedin_icon.svg';
